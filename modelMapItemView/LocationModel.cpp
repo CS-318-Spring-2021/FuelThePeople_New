977093b547f8504b9123e74eff1b8cebb46143c2
Roodharvens Joseph <76496598+roodharvensj@users.noreply.github.com>
2021-05-18 19:09:42 -0400

    Working importAPI, processAPI updated
    processAPI does not add to the map. Maybe make a new method to add all locations to the map at the same time? Maybe a signal/slot?

--- modelMapItemView/LocationModel.cpp
+++ modelMapItemView/LocationModel.cpp
@@ -13,43 +13,43 @@
 #include <QJsonDocument>
 #include <QVariant>
 
-//make a constructor
-//add to view
+
 
 LocationModel::LocationModel(QString _modelName, QString _filePath, QString _amenityType, QColor _color) :
     modelName(_modelName), filePath(_filePath), amenityType(_amenityType), color(_color)
 { }
 
-
-
+//populates map with points with attributes according to the model and the enumerated roles
 void LocationModel::addToMap(QQuickView &view) {
-
     //create an array to store each role
     QHash<int, QByteArray> roles;
     roles[CoordinateRole] = QByteArray("coordinate");
     roles[ColorRole] = QByteArray("color");
     roles[NameRole] = QByteArray("name");
     roles[WebsiteRole] = QByteArray("website");
+    roles[AmenityRole] = QByteArray("amenity");
     setItemRoleNames(roles);
 
     view.rootContext()->setContextProperty(modelName, this);
 
     if(filePath.startsWith("https")){
+        qDebug()<<"API Made It \n";
         importJSon();
     }else{
+        qDebug()<<"CSV Made It \n";
         importCSV();
     }
 }
 
 void LocationModel::importCSV(){
 #if 1
-
     QString data;
     QFile importedCSV(filePath);
     QStringList rowOfData;
     QStringList rowData;
     double latitude;
     double longitude;
+    QString amenityType;
     QString name;
     QString orderLink;
     data.clear();
@@ -67,24 +67,30 @@ void LocationModel::importCSV(){
     for (int x = 1; x < rowOfData.size(); x++)
     {
         rowData = rowOfData.at(x).split(",");
-        qDebug()<<rowData[3]<<amenityType;
         //filter by amenity type
-        if (rowData[3] == amenityType) {
-            latitude= rowData[0].toDouble();
-            longitude= rowData[1].toDouble();
-            name = rowData[2];
-            orderLink = rowData[8];
+        //        if (rowData[3] == amenityType) {
+        //qDebug()<<rowData[3]<<amenityType;
+        //filter by amenity type, move to QML in order to have the drop down menu choose what is displayed?
+        //if (rowData[3] == amenityType) {
+        latitude= rowData[0].toDouble();
+        longitude= rowData[1].toDouble();
+        name = rowData[2];
+        orderLink = rowData[8];
+        amenityType = rowData[3];
 
-            //uses QStandardItem to assign attributes from the CSV to roles
-            QStandardItem *item = new QStandardItem;
-            item->setData(QVariant::fromValue(name), NameRole);
-            item->setData(QVariant::fromValue(orderLink), WebsiteRole);
-            item->setData(QVariant::fromValue(QGeoCoordinate(latitude, longitude)), CoordinateRole);
-            item->setData(QVariant::fromValue(color), ColorRole);
-            appendRow(item);
-        }
+        //uses QStandardItem to assign attributes from the CSV to roles
+        QStandardItem *item = new QStandardItem;
+        item->setData(QVariant::fromValue(name), NameRole);
+
+        item->setData(QVariant::fromValue(amenityType), AmenityRole);
+        item->setData(QVariant::fromValue(orderLink), WebsiteRole);
+        item->setData(QVariant::fromValue(QGeoCoordinate(latitude, longitude)), CoordinateRole);
+        item->setData(QVariant::fromValue(color), ColorRole);
+        appendRow(item);
+        //        }
 
     }
+
 #endif
 
 }
@@ -92,55 +98,90 @@ void LocationModel::importCSV(){
 
 void LocationModel::importJSon(){
     QUrl myUrl= filePath;
-    if(myUrl.hasQuery()){
-        myUrl.setQuery("New York City");
-    }
     newFile = new FileDownloader(myUrl,this);
     connect(newFile, SIGNAL (downloaded()), this, SLOT (processData()));
-
-
+    qDebug()<<"File Sent";
 }
 
 void LocationModel::processData(){
     QJsonDocument itemDoc = QJsonDocument::fromJson(newFile->downloadedData());
-    QJsonArray latArr =itemDoc["Latitude"].toArray();
-    QJsonArray lngArr =itemDoc["Longitude"].toArray();
-    QJsonArray titleArr =itemDoc["title"].toArray();
-    QJsonArray addressArr =itemDoc["address"].toArray();
-    QJsonArray contactArr =itemDoc["contact"].toArray();
+    const QJsonValue &dataSet= itemDoc["data"].toArray();
+    bool end=false;
+    int i=0;
+    qDebug()<<"Did We Make It?";
+    while(end==false){
+        qDebug()<<"1";
+        QJsonValue datapoint= dataSet[i];
+        QJsonValue information= datapoint["source"];
+        QJsonValue locationData= information["data"];
+        while(locationData["city"].toString()!="New York City"){
+            i=i+1;
+            datapoint= dataSet[i];
+            information= datapoint["source"];
+            locationData= information["data"];
+        }
 
-    for(int x=0;x<titleArr.size(); x++){
-        LocationRecord record("Latitude",latArr[x].toVariant());
-        record.addVal("Longitude",latArr[x].toVariant());
-        record.addVal("title",titleArr[x].toVariant());
-        record.addVal("address",addressArr[x].toVariant());
-        record.addVal("contact",contactArr[x].toVariant());
+
+
+        LocationRecord record("latitude",locationData["lat"]);
+        record.addVal("longitude",locationData["lng"]);
+        record.addVal("title",locationData["title"]);
+        record.addVal("address",locationData["address"]);
+        //record.addVal("contact",datapoint["contact"]["general"]["web"]);
+
+        //qDebug()<<(record.getVal("contact")).toString();
 
         QStandardItem *item = new QStandardItem;
-        item->setData(QVariant::fromValue(QGeoCoordinate(latArr[x].toDouble(), lngArr[x].toDouble())), CoordinateRole);
+        item->setData(QVariant::fromValue(QGeoCoordinate(record.getVal("latitude").toDouble(), record.getVal("longitude").toDouble())), CoordinateRole);
         item->setData(QVariant::fromValue(color), ColorRole);
         appendRow(item);
 
         records.insert(records.end(),record);
+        i=i+1;
+        if(dataSet[i].isNull() || i==100){
+            end=true;
+            qDebug()<<"We Made It?";
+
+        }
     }
 
-    }
+}
 
 
 
 
+    //    QJsonValue latArr =itemDoc["latlng:latitude"].toArray();
+    //    QJsonArray lngArr =itemDoc["longitude"].toArray();
+    //    QJsonArray titleArr =itemDoc["title"].toArray();
+    //    QJsonArray addressArr =itemDoc["address"].toArray();
+    //    QJsonArray contactArr =itemDoc["contact"].toArray();
+    //    QJsonArray cityArr = itemDoc["city"].toArray();
+    //    qDebug()<<titleArr.isEmpty();
+    //    qDebug()<<latArr.isEmpty();
+    //    qDebug()<<lngArr.isEmpty();
+    //    qDebug()<<addressArr.isEmpty();
+    //    qDebug()<<contactArr.isEmpty();
 
 
-//std::list<LocationRecord> LocationModel:: getLocationRecordList(){
-//    return records;
-//}
 
-//void LocationModel:: appendLocationRecordList(QVariant data){
+    //    for(int x=0;x<titleArr.size(); x++){
+    //        if(cityArr[x].toString()!="New York City"){
+    //            break;
+    //        }
+    //        qDebug()<<"Did We Make It?";
 
+    //        LocationRecord record("latitude",latArr[x].toVariant());
+    //        record.addVal("longitude",latArr[x].toVariant());
+    //        record.addVal("title",titleArr[x].toVariant());
+    //        record.addVal("address",addressArr[x].toVariant());
+    //        record.addVal("contact",contactArr[x].toVariant());
 
-//}
+    //        qDebug()<<(record.getVal("title")).toString();
 
-//LocationRecord LocationModel:: getLocationRecord(QString id){
-//    return records.at(id) ;
-//}
+    //        QStandardItem *item = new QStandardItem;
+    //        item->setData(QVariant::fromValue(QGeoCoordinate(latArr[x].toDouble(), lngArr[x].toDouble())), CoordinateRole);
+    //        item->setData(QVariant::fromValue(color), ColorRole);
+    //        appendRow(item);
+    //        qDebug()<<"\n We Made It \n";
 
+    //        records.insert(records.end(),record);
